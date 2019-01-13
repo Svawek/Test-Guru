ba6b95d5a7e3054b1804d8a03639f4115c918574
svawek <svawek@gmail.com>
2019-01-13 22:22:21 +0300

    Add default values

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_13_190520) do
+ActiveRecord::Schema.define(version: 2019_01_13_191854) do
 
   create_table "answers", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "question_id", null: false
     t.string "user_answer", null: false
-    t.boolean "correct"
+    t.boolean "correct", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["question_id"], name: "index_answers_on_question_id"
@@ -40,7 +40,7 @@ ActiveRecord::Schema.define(version: 2019_01_13_190520) do
 
   create_table "tests", force: :cascade do |t|
     t.string "title", null: false
-    t.integer "level", null: false
+    t.integer "level", default: 1, null: false
     t.integer "category_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
