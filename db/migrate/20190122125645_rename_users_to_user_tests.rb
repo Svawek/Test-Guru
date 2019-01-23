c3a45fa5e2fee0843fbefb22d2deb0fb113b6e3c
Svawek <svawek@gmail.com>
2019-01-23 10:38:49 +0300

    some fixes of models

--- db/migrate/20190122125645_rename_users_to_user_tests.rb
+++
@@ -1,5 +0,0 @@
-class RenameUsersToUserTests < ActiveRecord::Migration[5.2]
-  def change
-    rename_table :users, :user_tests
-  end
-end
--- db/migrate/20190122130625_rename_column_user_id_to_user_test_id.rb
+++
@@ -1,5 +0,0 @@
-class RenameColumnUserIdToUserTestId < ActiveRecord::Migration[5.2]
-  def change
-    rename_column :opened_tests, :user_id, :user_test_id
-  end
-end
