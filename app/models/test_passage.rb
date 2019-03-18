9234521ae7e7ad7dcdf9b5ef555e435ad53bdeda
svawek <svawek@gmail.com>
2019-03-18 10:17:15 +0300

    fix test_passage callbacks

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -3,24 +3,16 @@ class TestPassage < ApplicationRecord
   belongs_to :test
   belongs_to :current_question, class_name: 'Question', optional: true
 
-  before_validation :before_validation_set_first_question, on: :create
-  before_validation :before_validation_set_correct_answers, on: :create
-  before_validation :before_validation_set_questions_amount, on: :create
-  before_validation :before_validation_set_current_question_number, on: :create
-  before_update :before_update_next_question, on: :update
+  before_validation :before_validation_set_next_question
 
-  PASSING_SCORE = 85.freeze
+  PASSING_SCORE = 85
 
-  @@correct_test_answers = 0
-  @@questions_amount = 0
-  @@current_question_number = 0
 
   def accept!(answer_ids)
     if correct_answer?(answer_ids)
       self.correct_questions += 1
     end
 
-    @@current_question_number += 1
     save!
   end
 
@@ -29,7 +21,7 @@ class TestPassage < ApplicationRecord
   end
 
   def result_percent
-    (correct_questions / @@correct_test_answers * 100).round(2)
+    (correct_questions / test.questions.count * 100).round(2)
   end
 
   def passed?
@@ -37,44 +29,30 @@ class TestPassage < ApplicationRecord
   end
 
   def show_progress
-    @@current_question_number
+    test.questions.index(current_question) + 1
   end
 
   def show_questions_amount
-    @@questions_amount
+    test.questions.count
   end
 
   private
 
-  def before_validation_set_first_question
-    self.current_question = test.questions.first if test.present?
-  end
-
-  def before_validation_set_correct_answers
-    @@correct_test_answers = 0
+  def before_validation_set_next_question
+    if current_question.nil?
+      self.current_question = test.questions.first if test.present?
+    else
+      self.current_question = test.questions.order(:id).where('id > ?', current_question.id).first
+    end
   end
 
   def correct_answer?(answer_ids)
     correct_answer_count = correct_answer.count
-    @@correct_test_answers += correct_answer_count
     (correct_answer_count == correct_answer.where(id: answer_ids).count) &&
     correct_answer_count == answer_ids.count
   end
 
   def correct_answer
     current_question.answers.correct
-  end
-
-  def before_update_next_question
-    fol_question = test.questions.order(:id).where('id > ?', current_question.id).first
-    self.current_question = fol_question
-  end
-
-  def before_validation_set_questions_amount
-    @@questions_amount = test.questions.count
-  end
-
-  def before_validation_set_current_question_number
-    @@current_question_number = 1
   end
 end
