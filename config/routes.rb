1dd96b0329bcee00f610a6c07a9a6c6848191be7
Svawek <svawek@gmail.com>
2019-04-04 16:39:53 +0300

    fix flash sign in admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users, path: :gurus, path_names: { sign_in: :login, sign_out: :logout }
+  devise_for :users, path: :gurus, path_names: { sign_in: :login, sign_out: :logout }, controllers: {sessions: "sessions"}
  
   authenticated :user, lambda {|u| u.type == "Admin"} do
     root to: "admin/tests#index"
