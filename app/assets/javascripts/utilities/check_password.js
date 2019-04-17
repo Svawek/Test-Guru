816ffbf68d395848afae086ecca4597aa99ebe9f
Svawek <svawek@gmail.com>
2019-04-17 17:02:15 +0300

    Add checking pass in registration page

---
+++ app/assets/javascripts/utilities/check_password.js
@@ -0,0 +1,12 @@
+var check = function () {
+    var pass = document.getElementById('user_password')
+    var passConfirm = document.getElementById('user_password_confirmation')
+
+    if (passConfirm.value.length > 0) {
+        if (pass.value == passConfirm.value) {
+            return passConfirm.style.borderColor = 'green'
+        } else {
+            return passConfirm.style.borderColor = 'red'
+        }
+    }
+}
--- app/assets/javascripts/utilities/sorting.js
+++ app/assets/javascripts/utilities/sorting.js
@@ -1,41 +1,42 @@
-document.addEventListener('turbolinks:load', function() { // ждем пока загрзится страница
-    var control = document.querySelector('.sort-by-title') // выбираем все элементы с классом sort-by-title
-    if (control) {control.addEventListener('click', sortRowsByTitle)} // после клика на элемент запускается функция sortRowsByTitle, если control сучествует, то будем вешать обработчик событий
+document.addEventListener('turbolinks:load', function() {
+    var control = document.querySelector('.sort-by-title')
+    if (control) {control.addEventListener('click', sortRowsByTitle)}
 })
 function sortRowsByTitle() {
-    var table = document.querySelector('table') //находим таблицу нужную
+    var table = document.querySelector('table')
     //NodeList
-    var rows = table.querySelectorAll('tr') // находим все строки
+    var rows = table.querySelectorAll('tr')
     var sortedRows = []
 
     for (var i = 1; i < rows.length; i++) {
-        sortedRows.push(rows[i]) //все строки переместим в sortedRows для дальнейшей сортировки кроме заголовка
+        sortedRows.push(rows[i])
     }
 
-    if (this.querySelector('.octicom-arrow-up').classList.contains ('hide')){
-        sortedRows.sort(CpnpareRowsAsc)
-        this.querySelecor('.octicon-arrow-up').classList.remove('hidde')
-        this.querySelecor('.octicon-arrow-down').classList.add('hidde')
+    if (this.querySelector('.octicon-arrow-up').classList.contains ('hide')){
+        sortedRows.sort(compareRowsAsc)
+        this.querySelector('.octicon-arrow-up').classList.remove('hide')
+        this.querySelector('.octicon-arrow-down').classList.add('hide')
     } else {
-        sortedRows.sort(CpnpareRowsDesc)
-        this.querySelecor('.octicon-arrow-up').classList.remove('hidde')
-        this.querySelecor('.octicon-arrow-down').classList.add('hidde')
-// проверяет есть ли в классе octicom-arrow-up, а в нем класс hide. и потом выбирает сортировку
+        sortedRows.sort(compareRowsDesc)
+        this.querySelector('.octicon-arrow-down').classList.remove('hide')
+        this.querySelector('.octicon-arrow-up').classList.add('hide')
     }
 
     var sortedTable = document.createElement('table')
-    sortedTable.classList.add('table') // создаем новую таблицу
-    sortedTable.appendChild(rows[0]) // помещаем зоголовок в новую таблицу
-
+    sortedTable.classList.add('table')
+    var theadRow = rows[0]
+    theadRow.classList.add('thead-dark')
+    sortedTable.appendChild(theadRow)
+    
     for(var i = 0; i < sortedRows.length; i++) {
-        sortedTable.appendChild(sortedRows[i]) // добавляем в новую таблицу отсортированные строки
+        sortedTable.appendChild(sortedRows[i])
     }
 
-    table.parentNode.replaceChild(sortedTable, table) // убираем старую таблицу со страницы и рендерим новую
+    table.parentNode.replaceChild(sortedTable, table)
 }
 function compareRowsAsc(row1, row2) {
     var testTitle1 = row1.querySelector('td').textContent
-    var testTitle1 = row2.querySelector('td').textContent
+    var testTitle2 = row2.querySelector('td').textContent
 
     if (testTitle1 < testTitle2) {return -1}
     if (testTitle1 > testTitle2) {return 1}
@@ -44,14 +45,9 @@ function compareRowsAsc(row1, row2) {
 
 function compareRowsDesc(row1, row2) { //
     var testTitle1 = row1.querySelector('td').textContent
-    var testTitle1 = row2.querySelector('td').textContent
+    var testTitle2 = row2.querySelector('td').textContent
 
     if (testTitle1 < testTitle2) {return 1}
     if (testTitle1 > testTitle2) {return -1}
     return 0
-}
-
-
-.hide {
-    display: none;
 }
