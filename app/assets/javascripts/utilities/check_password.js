d160c871b92b6a92c37e643fc4e38265d9943300
svawek@ <svawek@gmail.com>
2019-04-18 21:57:03 +0300

    Fix checking password

--- app/assets/javascripts/utilities/check_password.js
+++ app/assets/javascripts/utilities/check_password.js
@@ -1,6 +1,7 @@
 var check = function () {
     var pass = document.getElementById('user_password')
     var passConfirm = document.getElementById('user_password_confirmation')
+    passConfirm.style.borderColor = 'initial'
 
     if (passConfirm.value.length > 0) {
         if (pass.value == passConfirm.value) {
