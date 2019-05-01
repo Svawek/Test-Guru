bc8cbb77a317ad8d191d246c61d80cffac6f8bac
svawek@ <svawek@gmail.com>
2019-05-01 12:48:14 +0300

    change Action Mailer smtp settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,9 +65,11 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
+    domain: 'test-guru-svawek.herokuapp.com',
     user_name: ENV['SMTP_USERNAME'],
     password: ENV['SMTP_PASSWORD'],
     authentication: 'plain',
